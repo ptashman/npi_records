7676b7cdc353eb8262c77b60b5f4bf3456390b79
Paul Tashman <paultashman@gmail.com>
2022-02-11 13:20:18 -0500

    tweak to update

--- app/models/npi_record.rb
+++ app/models/npi_record.rb
@@ -6,7 +6,7 @@ class NpiRecord < ApplicationRecord
   def self.update_or_create_by_identifier!(identifier)
     api_record_data = NpiRegistryService.new(identifier).get_record_data.first.with_indifferent_access
     record = find_or_create_by!(identifier: identifier)
-    record.update!(npi_record_data(api_record_data))
+    record.update!(npi_record_data(api_record_data).merge(updated_at: DateTime.now))
     api_record_data[:addresses].each { |address| Address.create!(address.merge(npi_record: record)) }
     api_record_data[:taxonomies].each { |taxonomy| Taxonomy.create!(taxonomy.merge(npi_record: record)) }
     api_record_data[:identifiers].each { |identifier| Identifier.create!(identifier.merge(npi_record: record)) }
